e7f46a9fd4ee232313ee2c7e865888bb2d7ad474
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-05-14 19:50:49 +0000

    added safe area context

---
+++ App.style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+    app: {
+        flex: 1,
+        backgroundColor: "#f9f9f9",
+        justifyContent: "space-evenly",
+        padding: 16,
+    },
+});

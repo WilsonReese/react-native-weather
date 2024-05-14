9ec610dccce01f11216a506ad051f861a5669063
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-05-14 19:58:11 +0000

    got home page to show up

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { Text, View } from 'react-native';
 import { s } from "./App.style";
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Home } from './pages/Home/Home';
 
 export default function App() {
   return (
     <>
       <SafeAreaProvider>
         <SafeAreaView style={s.app}>
-          <Text>Hello</Text>
+          <Home/>
         </SafeAreaView>
       </SafeAreaProvider>
     </>
---
+++ pages/Home/Home.jsx
@@ -0,0 +1,6 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { s } from "./Home.style";
+
+export function Home({ }) {
+  return <Text style={s.text}>Home</Text>;
+}

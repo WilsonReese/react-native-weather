4f5650fd3c0c4b15e2f53a1b01f1035dfd5161e3
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-05-15 17:39:36 +0000

    got location working

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
-import { ImageBackground, Text, View } from 'react-native';
+import { ImageBackground, Text, View } from "react-native";
 import { s } from "./App.style";
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { Home } from './pages/Home/Home';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Home } from "./pages/Home/Home";
 import backgroundImage from "./assets/background.png";
-
-
+import { useEffect, useState } from "react";
+import {
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync,
+} from "expo-location";
 
 export default function App() {
-    
+  const [coordinates, setCoordinates] = useState();
 
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        long: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: "48.85", long: "2.35" });
+    }
+  }
+
+  console.log(coordinates)
 
   return (
-    <ImageBackground imageStyle={s.img} style={s.img_background_view} source={backgroundImage}>
+    <ImageBackground
+      imageStyle={s.img}
+      style={s.img_background_view}
+      source={backgroundImage}
+    >
       <SafeAreaProvider>
         <SafeAreaView style={s.container}>
-          <Home/>
+          <Home />
         </SafeAreaView>
       </SafeAreaProvider>
     </ImageBackground>
   );
 }
-
